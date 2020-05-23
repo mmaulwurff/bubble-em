b4204f1bfc00b3d1d939bc8b88cc0f21d6f685b6
Alexander Kromm <mmaulwurff@gmail.com>
2020-05-23 17:40:54 +0700

    add version to build

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-#filename=10.5x-$(git describe --abbrev=0 --tags).pk3
-filename=bubble-em.pk3
+filename=bubble-em-$(git describe --abbrev=0 --tags).pk3
 
 rm -f  $filename
 zip -R $filename "*.md" "*.txt" "*.zs" "*.png" "*.ogg"
